a909771409bab971e4b7537619abaa9a7245c664
Joe <SuperSpyTX@users.noreply.github.com>
2017-08-13 16:34:13 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+if ($_GET['test']) {
+  echo "This is my input: ".$_GET['test']."\n\n\n\n\n";
+}
+
 echo "Hello World I'm a docker test I'm going to get ripperoni";
 
 echo "</br></br>Finally, it WORKS.  I can go to bed now yay.";
